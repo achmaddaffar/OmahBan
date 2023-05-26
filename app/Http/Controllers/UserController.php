7508272c970731d8133179b857278cac090a84cb
damieroliverroot <daffarres@gmail.com>
2023-05-26 23:01:19 +0700

    implement login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -22,16 +23,34 @@ class UserController extends Controller
             'confirm_password' => 'required|same:password',
         ]);
         $user = new User([
-            'name'=> $request->name,
+            'name' => $request->name,
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
         $user->save();
-        return redirect()->route('login')->with('success', 'Registration is Successful, Please Login!');
+        return redirect()->route('user.login')->with('success', 'Registration is Successful, Please Login!');
     }
 
-    public function login() {
+    public function login()
+    {
         $data['title'] = 'Login';
         return view('login.index', $data);
+    }
+
+    public function loginAction(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' =>'required',
+        ]);
+
+        if (Auth::attempt([
+            'email' => $request->email,
+            'password' => $request->password,
+        ])) {
+            $request->session()->regenerate();
+            return redirect()->intended('dashboard');
+        }
+        return back()->withErrors('password', 'Wrong email or password!');
     }
 }
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,12 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
-</head>
-<body>
-    
-</body>
-</html>
+@extends('app')
+@section('content')
+    <div class="row">
+        <div class="col-md-6">
+            @if (session('success'))
+                <p class="alert alert-success">{{ session('success') }}</p>
+            @endif
+            @if ($errors->any())
+                @foreach ($errors->all() as $err)
+                    <p class="alert alert-danger">{{ $err }}</p>
+                @endforeach
+            @endif
+            <form method="POST" action="{{ route('login.action') }}">
+                @csrf
+                <div class="nb-3">
+                    <label>Email <span class="text-danger">*</span></label>
+                    <input class="form-control" type="email" name="email" value="{{ old('email') }}" />
+                </div>
+                <div class="nb-3">
+                    <label>Password <span class="text-danger">*</span></label>
+                    <input class="form-control" type="password" name="password" />
+                </div>
+                <div class="nb-3">
+                    <button class="btn btn-primary">
+                        Login
+                    </button>
+                    <a class="btn btn-danger" href="{{ route('home') }}">Back</a>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
