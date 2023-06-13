8cbd7a3b48ccc41ca2a4228052f1a32c14a14e2c
RushingAlien <rushing27alien@gmail.com>
2023-06-13 13:44:47 +0700

    Make index of each page use list.layout

--- resources/views/struk/index.blade.php
+++ resources/views/struk/index.blade.php
@@ -1,41 +1,29 @@
-@extends('layouts.app')
+@extends('layouts.list')
 
 @section('title', 'Data Struk')
-@section('contents')
-    <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Data Struk</h6>
-        </div>
-        <div class="card-body">
-            <a href="{{ route('struk.tambah') }}" class="btn btn-primary mb-3">Tambah</a>
-            <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>ID Struk</th>
-                            <th>ID Pembeli</th>
-                            <th>Tanggal Transaksi</th>
-                            <th>Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @php($no = 1)
-                        @foreach ($struk as $row)
-                            <tr>
-                                <th>{{ $no++ }}</th>
-                                <td>{{ $row->id_struk }}</td>
-                                <td>{{ $row->id_pembeli }}</td>
-                                <td>{{ $row->created_at }}</td>
-                                <td>
-                                    <a href="{{ route('struk.edit', $row->id) }}" class="btn btn-warning">edit</a>
-                                    <a href="{{ route('struk.hapus', $row->id) }}" class="btn btn-danger">hapus</a>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
+@section('tambah-rute')
+    {{ route('struk.tambah') }}
+@endsection
+
+@section('column-names')
+    <th>No</th>
+    <th>ID Struk</th>
+    <th>ID Pembeli</th>
+    <th>Tanggal Transaksi</th>
+    <th>Aksi</th>
+@endsection
+@section('table')
+    @php($no = 1)
+    @foreach ($struk as $row)
+        <tr>
+            <th>{{ $no++ }}</th>
+            <td>{{ $row->id_struk }}</td>
+            <td>{{ $row->id_pembeli }}</td>
+            <td>{{ $row->created_at }}</td>
+            <td>
+                <a href="{{ route('struk.edit', $row->id) }}" class="btn btn-warning">edit</a>
+                <a href="{{ route('struk.hapus', $row->id) }}" class="btn btn-danger">hapus</a>
+            </td>
+        </tr>
+    @endforeach
 @endsection
